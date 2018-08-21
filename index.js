5c89a59634f77331205d69daf855f3364066a676
davidr123 <davidradaalejandro@gmail.com>
2018-08-21 11:17:57 -0500

    commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000
+
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res) => res.render('home'));
+
+
+app.listen(PORT, () => console.log(`express on location ${PORT}`));
+
+/*
+
+app.set('port', process.env.PORT || 4000);
+
+
+app.get('/', function(req, res){
+    res.type('text/plain');
+    res.send('mi pagina principal');
+});
+
+app.get('/about', function(req, res){
+    res.type('text/plain');
+    res.send('acercade mi pagina');
+});
+
+
+
+app.get('/about/contact', function(req, res){
+    res.type('text/plain');
+    res.send('acercade mi pagina');
+});
+
+
+app.use( function(req,res){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res,  next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - server error');
+});
+
+
+
+
+
+
+app.listen(app.get('port'), function(){
+           
+           console.log('express on localhost:' + app.get('port'));
+    
+           });
+*/
